c4937f2490b2f12f5597869ab7af061b723724f5
Your Name <you@example.com>
2020-08-13 02:49:44 +0300

    qwe

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-   name='medcon',
-   version='1.0',
-   description='play series episode by episode',
-   install_requires=requirements,
-   python_requires='>=3.6',
+    name='medcon-cryacrya',
+    version='1.2',
+    description='play series episode by episode',
+    install_requires=requirements,
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+    scripts=['scripts/medcon']
 )
